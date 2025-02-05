d992348a8a40412ac9122b55e663d068b65edf0b
Kalyan Chavali <kalyan.chavali123@gmail.com>
2025-02-04 21:41:17 -0800

    added basic get recipe API

--- src/main/java/com/wb/culinaryCode/controller/RecipeController.java
+++ src/main/java/com/wb/culinaryCode/controller/RecipeController.java
@@ -1,14 +1,29 @@
 package com.wb.culinaryCode.controller;
 
+import com.wb.culinaryCode.model.Recipe;
+import com.wb.culinaryCode.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeController {
 
+    @Autowired
+    private RecipeService recipeService;
+
     @GetMapping("/")
     public ResponseEntity<String> getWelcomeMessage() {
         return ResponseEntity.ok("Welcome to Culinary Code!");
+    }
+
+    @GetMapping("/recipe/{recipeId}")
+    public ResponseEntity<Recipe> getRecipeById(@PathVariable Long recipeId) {
+        var recipeOpt = recipeService.getRecipeById(recipeId);
+
+        return recipeOpt.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/wb/culinaryCode/service/RecipeService.java
+++ src/main/java/com/wb/culinaryCode/service/RecipeService.java
@@ -1,7 +1,19 @@
 package com.wb.culinaryCode.service;
 
+import com.wb.culinaryCode.dao.RecipeRepository;
+import com.wb.culinaryCode.model.Recipe;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RecipeService {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    public Optional<Recipe> getRecipeById(Long recipeId){
+        return recipeRepository.findById(recipeId);
+    }
 }
